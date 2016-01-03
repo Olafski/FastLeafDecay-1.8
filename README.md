5dc5c5016ce620a0e8d37deac2b498cf223a9753
Olaf Keijsers <olaf@keijsers.org>
2016-01-03 13:00:01 +0100

    Minecraft 1.8.9 support

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Download the source code from right here, or get the jar version from [CurseForg
 Comments are welcome on CurseForge or in the [MinecraftForum thread](http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/2196219-fastleafdecay-leaves-be-gone). Bug reports may also be entered here on GitHub.
 
 ## Requirements
-* Minecraft 1.8 or 1.8.8
+* Minecraft 1.8 or 1.8.8 or 1.8.9
 * [Minecraft Forge](http://files.minecraftforge.net/) version 1.8 - 11.14.4.1577 or higher
  
 Only servers need to have this mod, so if you're playing on a server that has it installed you will not need to install it on the client.
--- src/main/java/com/olafski/fastleafdecay/FastLeafDecay.java
+++ src/main/java/com/olafski/fastleafdecay/FastLeafDecay.java
@@ -6,10 +6,10 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 /**
  * Created by Olaf on 29-12-2015.
  */
-@Mod(modid = FastLeafDecay.MODID, version = FastLeafDecay.VERSION, acceptableRemoteVersions = "*", acceptedMinecraftVersions = "[1.8,1.8.8]")
+@Mod(modid = FastLeafDecay.MODID, version = FastLeafDecay.VERSION, acceptableRemoteVersions = "*", acceptedMinecraftVersions = "[1.8,1.8.9]")
 public class FastLeafDecay {
     public static final String MODID = "fastleafdecay";
-    public static final String VERSION = "1.5";
+    public static final String VERSION = "1.6";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
